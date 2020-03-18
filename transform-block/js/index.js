b7b001f678c084f1c3202c7fb3cbaa2344a0713d
Stark <59829975+StarkEspada@users.noreply.github.com>
2020-03-18 13:13:33 +0300

    Add files via upload

---
+++ transform-block/js/index.js
@@ -0,0 +1,325 @@
+
+var width = document.querySelector(".width")
+var height = document.querySelector(".height")
+var transform = document.querySelector(".transform")
+var transformRotateX = document.querySelector(".transform-rotate-x")
+var transformRotateY = document.querySelector(".transform-rotate-y")
+var transformRotate3d = document.querySelector(".transform-rotate-3d")
+var borderTopLeft = document.querySelector(".border-top-left")
+var borderTopRight = document.querySelector(".border-top-right")
+var borderBottomLeft = document.querySelector(".border-bottom-left")
+var borderBottomRight = document.querySelector(".border-bottom-right")
+var background = document.querySelector(".background")
+var blockShadow = document.querySelector(".box-shadow")
+var newBlock = document.querySelector(".new-block")
+var marginLeft = document.querySelector(".margin-left")
+var marginRight = document.querySelector(".margin-right")
+var marginTop = document.querySelector(".margin-top")
+var marginBottom = document.querySelector(".margin-bottom")
+
+
+
+
+var paramWidth = document.querySelector(".param-width")
+var paramHeight = document.querySelector(".param-height")
+var paramTransform = document.querySelector(".param-transform")
+var paramTransformRotateX = document.querySelector(".param-transform-rotate-x")
+var paramTransformRotateY = document.querySelector(".param-transform-rotate-y")
+var paramTransformRotate3dOne = document.querySelector(".param-transform-rotate-3d-one")
+var paramTransformRotate3dTwo = document.querySelector(".param-transform-rotate-3d-two")
+var paramTransformRotate3dTree = document.querySelector(".param-transform-rotate-3d-tree")
+var paramTransformRotate3dFor = document.querySelector(".param-transform-rotate-3d-for")
+var paramBorderRadiusTopLeft = document.querySelector(".param-border-radius-top-left")
+var paramBorderRadiusTopRight = document.querySelector(".param-param-border-radius-top-right")
+var paramBorderRadiusBottomLeft = document.querySelector(".param-border-radius-bottom-left")
+var paramBorderRadiusBottomRight = document.querySelector(".param-border-radius-bottom-right")
+var paramBackground = document.querySelector(".param-background")
+var paramBoxShadowOne = document.querySelector(".param-box-shadow-one")
+var paramBoxShadowTwo = document.querySelector(".param-box-shadow-two")
+var paramBoxShadowTree = document.querySelector(".param-box-shadow-tree")
+var paramBoxShadowFor = document.querySelector(".param-box-shadow-for")
+var paramBoxShadowColor = document.querySelector(".param-box-shadow-color")
+var paramMargiLeft = document.querySelector(".param-margin-left")
+var paramMarginRight = document.querySelector(".param-margin-right")
+var paramMarginTop = document.querySelector(".param-margin-top")
+var paramMarginBottom = document.querySelector(".param-margin-bottom")
+
+
+
+
+
+var show = document.querySelectorAll(".show")
+var update = document.querySelector(".update")
+var block = document.querySelector(".block")
+var line = document.querySelectorAll(".line")
+var container = document.querySelector(".container")
+var column = document.querySelector(".column")
+var count = 1
+
+newBlock.addEventListener("click", function(){
+	var blockTwo = document.createElement("div")
+	blockTwo.classList.add("blockOne")
+	container.appendChild(blockTwo)
+	count++
+
+		var inputOneBlockTwo = document.createElement("input")
+		var inputTwoBlockTwo = document.createElement("input")
+		var inputTreeBlockTwo = document.createElement("input")
+		var inputForBlockTwo = document.createElement("input")
+		var inputFiveBlockTwo = document.createElement("input")
+		var inputSixBlockTwo = document.createElement("input")
+		var inputSevenBlockTwo = document.createElement("input")
+		var inputEightBlockTwo = document.createElement("input")
+		var inputNineBlockTwo = document.createElement("input")
+		var inputTenBlockTwo = document.createElement("input")
+		var inputElevenOneBlockTwo = document.createElement("input")
+		var inputElevenTwoBlockTwo = document.createElement("input")
+		var inputElevenTreeBlockTwo = document.createElement("input")
+		var inputElevenForBlockTwo = document.createElement("input")
+		var inputTwelveOneBlockTwo = document.createElement("input")
+		var inputTwelveTwoBlockTwo = document.createElement("input")
+		var inputTwelveTreeBlockTwo = document.createElement("input")
+		var inputTwelveForBlockTwo = document.createElement("input")
+		var inputTwelveFiveBlockTwo = document.createElement("input")
+		var inputЕhirteenBlockTwo = document.createElement("input")
+		var inputFourteenBlockTwo = document.createElement("input")
+		var inputFifteenBlockTwo = document.createElement("input")
+		var inputSixteenBlockTwo = document.createElement("input")
+		var numberElement = document.createElement("div")
+
+		
+		
+
+			numberElement.classList.add("element-two")
+			inputOneBlockTwo.classList.add("param-width-two")
+			inputTwoBlockTwo.classList.add("param-width-two")
+			inputTreeBlockTwo.classList.add("param-width-two")
+			inputForBlockTwo.classList.add("param-width-two")
+			inputFiveBlockTwo.classList.add("param-width-two")
+			inputSixBlockTwo.classList.add("param-width-two")
+			inputSevenBlockTwo.classList.add("param-width-two")
+			inputEightBlockTwo.classList.add("param-width-two")
+			inputNineBlockTwo.classList.add("param-width-two")
+			inputTenBlockTwo.classList.add("param-width-two")
+			inputElevenOneBlockTwo.classList.add("param-width-two-b")
+			inputElevenTwoBlockTwo.classList.add("param-width-two-b")
+			inputElevenTreeBlockTwo.classList.add("param-width-two-b")
+			inputElevenForBlockTwo.classList.add("param-width-two-b")
+			inputTwelveOneBlockTwo.classList.add("param-width-two-b")
+			inputTwelveTwoBlockTwo.classList.add("param-width-two-b")
+			inputTwelveTreeBlockTwo.classList.add("param-width-two-b")
+			inputTwelveForBlockTwo.classList.add("param-width-two-b")
+			inputTwelveFiveBlockTwo.classList.add("param-box-shadow-color-two")
+			inputЕhirteenBlockTwo.classList.add("param-width-two")
+			inputFourteenBlockTwo.classList.add("param-width-two")
+			inputFifteenBlockTwo.classList.add("param-width-two")
+			inputSixteenBlockTwo.classList.add("param-width-two")
+		
+
+
+				width.appendChild(inputOneBlockTwo)
+				height.appendChild(inputTwoBlockTwo)
+				transform.appendChild(inputTreeBlockTwo)
+				transformRotateX.appendChild(inputForBlockTwo)
+				transformRotateY.appendChild(inputFiveBlockTwo)
+				borderTopLeft.appendChild(inputSixBlockTwo)
+				borderTopRight.appendChild(inputSevenBlockTwo)
+				borderBottomLeft.appendChild(inputEightBlockTwo)
+				borderBottomRight.appendChild(inputNineBlockTwo)
+				background.appendChild(inputTenBlockTwo)
+				transformRotate3d.appendChild(inputElevenOneBlockTwo)
+				transformRotate3d.appendChild(inputElevenTwoBlockTwo)
+				transformRotate3d.appendChild(inputElevenTreeBlockTwo)
+				transformRotate3d.appendChild(inputElevenForBlockTwo)
+				blockShadow.appendChild(inputTwelveOneBlockTwo)
+				blockShadow.appendChild(inputTwelveTwoBlockTwo)
+				blockShadow.appendChild(inputTwelveTreeBlockTwo)
+				blockShadow.appendChild(inputTwelveForBlockTwo)
+				blockShadow.appendChild(inputTwelveFiveBlockTwo)
+				marginLeft.appendChild(inputЕhirteenBlockTwo)
+				marginRight.appendChild(inputFourteenBlockTwo)
+				marginTop.appendChild(inputFifteenBlockTwo)
+				marginBottom.appendChild(inputSixteenBlockTwo)
+				column.appendChild(numberElement)
+
+
+					inputOneBlockTwo.setAttribute("placeholder", "Px")
+					inputTwoBlockTwo.setAttribute("placeholder", "Px")
+					inputTreeBlockTwo.setAttribute("placeholder", "Deg")
+					inputForBlockTwo.setAttribute("placeholder", "Deg")
+					inputFiveBlockTwo.setAttribute("placeholder", "Deg")
+					inputSixBlockTwo.setAttribute("placeholder", "Deg")
+					inputSevenBlockTwo.setAttribute("placeholder", "Deg")
+					inputEightBlockTwo.setAttribute("placeholder", "Deg")
+					inputNineBlockTwo.setAttribute("placeholder", "Deg")
+					inputTenBlockTwo.setAttribute("placeholder", "Color")
+					inputElevenOneBlockTwo.setAttribute("placeholder", "Deg")
+					inputElevenTwoBlockTwo.setAttribute("placeholder", "Deg")
+					inputElevenTreeBlockTwo.setAttribute("placeholder", "Deg")
+					inputElevenForBlockTwo.setAttribute("placeholder", "Deg")
+					inputTwelveOneBlockTwo.setAttribute("placeholder", "Px")
+					inputTwelveTwoBlockTwo.setAttribute("placeholder", "Px")
+					inputTwelveTreeBlockTwo.setAttribute("placeholder", "Px")
+					inputTwelveForBlockTwo.setAttribute("placeholder", "Px")
+					inputTwelveFiveBlockTwo.setAttribute("placeholder", "Color")
+					inputЕhirteenBlockTwo.setAttribute("placeholder", "Px")
+					inputFourteenBlockTwo.setAttribute("placeholder", "Px")
+					inputFifteenBlockTwo.setAttribute("placeholder", "Px")
+					inputSixteenBlockTwo.setAttribute("placeholder", "Px")
+
+
+					numberElement.innerHTML = count + "El."
+
+
+
+
+						var newLineWidth = document.createElement("div")
+						var newLineHeight = document.createElement("div")
+						var newLineTransform = document.createElement("div")
+						var newLineTransformRotateX = document.createElement("div")
+						var newLineTransformRotateY = document.createElement("div")
+						var newLineBorderTopLeft = document.createElement("div")
+						var newLineBorderTopRight = document.createElement("div")
+						var newLineBorderBottomLeft = document.createElement("div")
+						var newLineBorderBottomRight = document.createElement("div")
+						var newLineBackground = document.createElement("div")
+						var newLineTransformRotate3d = document.createElement("div")
+						var newLineBlockShadow = document.createElement("div")
+						var newLineMarginLeft = document.createElement("div")
+						var newLineMarginRight = document.createElement("div")
+						var newLineMarginTop = document.createElement("div")
+						var newLineMarginBottom = document.createElement("div")
+						var newLineColumn = document.createElement("div")
+
+
+						newLineWidth.classList.add("line")
+						newLineHeight.classList.add("line")
+						newLineTransform.classList.add("line")
+						newLineTransformRotateX.classList.add("line")
+						newLineTransformRotateY.classList.add("line")
+						newLineBorderTopLeft.classList.add("line")
+						newLineBorderTopRight.classList.add("line")
+						newLineBorderBottomLeft.classList.add("line")
+						newLineBorderBottomRight.classList.add("line")
+						newLineBackground.classList.add("line")
+						newLineTransformRotate3d.classList.add("line")
+						newLineBlockShadow.classList.add("line")
+						newLineMarginLeft.classList.add("line")
+						newLineMarginRight.classList.add("line")
+						newLineMarginTop.classList.add("line")
+						newLineMarginBottom.classList.add("line")
+						newLineColumn.classList.add("line")
+
+
+
+						width.appendChild(newLineWidth)
+						height.appendChild(newLineHeight)
+						transform.appendChild(newLineTransform)
+						transformRotateX.appendChild(newLineTransformRotateX)
+						transformRotateY.appendChild(newLineTransformRotateY)
+						borderTopLeft.appendChild(newLineBorderTopLeft)
+						borderTopRight.appendChild(newLineBorderTopRight)
+						borderBottomLeft.appendChild(newLineBorderBottomLeft)
+						borderBottomRight.appendChild(newLineBorderBottomRight)
+						background.appendChild(newLineBackground)
+						transformRotate3d.appendChild(newLineTransformRotate3d)
+						blockShadow.appendChild(newLineBlockShadow)
+						marginLeft.appendChild(newLineMarginLeft)
+						marginRight.appendChild(newLineMarginRight)
+						marginTop.appendChild(newLineMarginTop)
+						marginBottom.appendChild(newLineMarginBottom)
+						column.appendChild(newLineColumn)
+
+
+		update.addEventListener("click", function(){
+			var blockTwoInputOne = inputOneBlockTwo.value
+			blockTwo.style.width = blockTwoInputOne + "px"
+			var blockTwoInputTwo = inputTwoBlockTwo.value
+			blockTwo.style.height = blockTwoInputTwo + "px"
+			var blockTwoInputTree = inputTreeBlockTwo.value
+			console.log(blockTwoInputTree)
+			blockTwo.style.transform = "rotate(" + blockTwoInputTree + "deg)"
+			var blockTwoInputFor = inputForBlockTwo.value
+			blockTwo.style.transform = "rotateX(" + blockTwoInputFor + "deg)"
+			var blockTwoInputFive = inputFiveBlockTwo.value
+			blockTwo.style.transform = "rotateY(" + blockTwoInputFive + "deg)"
+			var blockTwoInputSix = inputSixBlockTwo.value
+			blockTwo.style.borderTopLeftRadius = blockTwoInputSix + "px"
+			var blockTwoInputSeven = inputSevenBlockTwo.value
+			blockTwo.style.borderTopRightRadius = blockTwoInputSeven + "px"
+			var blockTwoInputEight = inputEightBlockTwo.value
+			blockTwo.style.borderBottomLeftRadius = blockTwoInputEight + "px"
+			var blockTwoInputNine = inputNineBlockTwo.value
+			blockTwo.style.borderBottomRightRadius = blockTwoInputNine + "px"
+			var blockTwoInputTen = inputTenBlockTwo.value
+			blockTwo.style.background = blockTwoInputTen
+			var blockTwoInputElevenOne = inputElevenOneBlockTwo.value
+			var blockTwoInputElevenTwo = inputElevenTwoBlockTwo.value
+			var blockTwoInputElevenTree = inputElevenTreeBlockTwo.value
+			var blockTwoInputElevenFor = inputElevenForBlockTwo.value
+			blockTwo.style.transform = "rotate3d(" + blockTwoInputElevenOne + "," + blockTwoInputElevenTwo + "," + blockTwoInputElevenTree + "," + blockTwoInputElevenFor + "deg)"
+			var blockTwoInputTwelveOne = inputTwelveOneBlockTwo.value
+			var blockTwoInputTwelveTwo = inputTwelveTwoBlockTwo.value
+			var blockTwoInputTwelveTree = inputTwelveTreeBlockTwo.value
+			var blockTwoInputTwelveFor = inputTwelveForBlockTwo.value
+			var blockTwoInputTwelveFive = inputTwelveFiveBlockTwo.value
+			blockTwo.style.boxShadow = blockTwoInputTwelveOne + "px " + blockTwoInputTwelveTwo + "px " + blockTwoInputTwelveTree + "px " + blockTwoInputTwelveFor + "px " + blockTwoInputTwelveFive
+			var blockTwoMarginLeft = inputЕhirteenBlockTwo.value
+			blockTwo.style.marginLeft = blockTwoMarginLeft + "px"
+			var blockTwoMarginRight = inputFourteenBlockTwo.value
+			blockTwo.style.marginRight = blockTwoMarginRight + "px"
+			var blockTwoMarginTop = inputFifteenBlockTwo.value
+			blockTwo.style.marginTop = blockTwoMarginTop + "px"
+			var blockTwoMarginBottom = inputSixteenBlockTwo.value
+			blockTwo.style.marginBottom = blockTwoMarginBottom + "px"
+		})
+})
+
+	update.addEventListener("click", function(){
+		
+		var inputWidth = paramWidth.value
+		block.style.width = inputWidth + "px"
+		var inputHeight = paramHeight.value
+		block.style.height = inputHeight + "px"
+		var inputTransform = paramTransform.value
+		block.style.transform = "rotate(" + inputTransform + "deg)" 
+		var inputBorderRadiusTopLeft = paramBorderRadiusTopLeft.value
+		block.style.borderTopLeftRadius = inputBorderRadiusTopLeft + "px"
+		var inputBorderRadiusTopRight = paramBorderRadiusTopRight.value
+		block.style.borderTopRightRadius = inputBorderRadiusTopRight + "px"
+		var inputBorderRadiusBottomLeft = paramBorderRadiusBottomLeft.value
+		block.style.borderBottomLeftRadius = inputBorderRadiusBottomLeft + "px"
+		var inputBorderRadiusBottomLeft = paramBorderRadiusBottomRight.value
+		block.style.borderBottomRightRadius = inputBorderRadiusBottomLeft + "px"
+		var inputTransformRotateX = paramTransformRotateX.value
+		block.style.transform = "rotateX(" + inputTransformRotateX + "deg)" 
+		var inputTransformRotateY = paramTransformRotateY.value
+		block.style.transform = "rotateY(" + inputTransformRotateY + "deg)"
+		var inputOneTransformRotate3d = paramTransformRotate3dOne.value
+		var inputTwoTransformRotate3d = paramTransformRotate3dTwo.value
+		var inputTreeTransformRotate3d = paramTransformRotate3dTree.value
+		var inputForTransformRotate3d = paramTransformRotate3dFor.value
+		block.style.transform = "rotate3d(" + inputOneTransformRotate3d + "," + inputTwoTransformRotate3d + "," + inputTreeTransformRotate3d + "," + inputForTransformRotate3d + "deg)" 
+		var inputBackground = paramBackground.value
+		block.style.background = inputBackground
+		var inputBoxShadowOne = paramBoxShadowOne.value
+		var inputBoxShadowTwo = paramBoxShadowTwo.value
+		var inputBoxShadowTree = paramBoxShadowTree.value
+		var inputBoxShadowFor = paramBoxShadowFor.value
+		var inputBoxShadowColor = paramBoxShadowColor.value
+		block.style.boxShadow = inputBoxShadowOne + "px " + inputBoxShadowTwo + "px " + inputBoxShadowTree + "px " + inputBoxShadowFor + "px " + inputBoxShadowColor
+		var inputMarginLeft = paramMargiLeft.value
+		console.log(inputMarginLeft)
+		block.style.marginLeft = inputMarginLeft + "px"
+		var inputMarginRight = paramMarginRight.value
+		block.style.marginRight = inputMarginRight + "px"
+		var inputMarginTop = paramMarginTop.value
+		block.style.marginTop = inputMarginTop + "px"
+		var inputMarginBottom = paramMarginBottom.value
+		block.style.marginBottom = paramMarginBottom + "px"
+
+
+	})
+
+
+
